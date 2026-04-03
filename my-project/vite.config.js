26daad6df031af5c00a8cd1822d1dd6403330019
rustyrory <marada49@live.fr>
2026-04-04 00:50:30 +0200

    App

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,49 +10,6 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'icons/icon.svg'],
-      manifest: {
-        name: 'B3dev Chat',
-        short_name: 'B3dev',
-        description: 'Application de tchat temps réel et gestion de données',
-        theme_color: '#111827',
-        background_color: '#f9fafb',
-        display: 'standalone',
-        start_url: '/B3dev-TP_VUE/',
-        scope: '/B3dev-TP_VUE/',
-        lang: 'fr',
-        icons: [
-          {
-            src: 'icons/icon.svg',
-            sizes: 'any',
-            type: 'image/svg+xml',
-            purpose: 'any'
-          },
-          {
-            src: 'icons/icon.svg',
-            sizes: 'any',
-            type: 'image/svg+xml',
-            purpose: 'maskable'
-          }
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,svg,woff2}'],
-        navigateFallback: '/B3dev-TP_VUE/index.html',
-        navigateFallbackDenylist: [
-          /^\/B3dev-TP_VUE\/api\//,
-          /^\/B3dev-TP_VUE\/socket\.io\//
-        ],
-        runtimeCaching: [
-          {
-            urlPattern: /^\/B3dev-TP_VUE\/api\//,
-            handler: 'NetworkOnly'
-          }
-        ]
-      }
-    })
   ],
   resolve: {
     alias: {
